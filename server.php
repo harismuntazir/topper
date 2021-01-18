c705d16cbd83c4a85bef46fe44f424c3161b0cd8
harismuntazir <haris.0813.m@gmail.com>
2021-01-18 14:42:14 +0530

    Update server.php

--- server.php
+++ server.php
@@ -6,7 +6,7 @@
  * Time: 10:55 PM
  */
 
-$mysql = mysqli_connect("localhost","root","","topper");
+$mysql = mysqli_connect("remotemysql.com","0UlclGY7Yx","0UlclGY7Yx","topper");
 
 if (!$mysql) {
     echo ucwords("<section id='msg'><p class='err_msg'> oops ! server connection error, check back later. </p></section>");
